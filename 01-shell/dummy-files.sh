cccea3725e14c694eca71049083354ebd9fbb3b4
anil kumar <akthinq@gmail.com>
2025-11-14 16:40:51 +0530

    backup-logs

--- 01-shell/dummy-files.sh
+++ 01-shell/dummy-files.sh
@@ -3,3 +3,7 @@
 sudo mkdir -p /var/log/app
 
 echo "created directory"
+
+sudo su -c "echo 'Error: Something failed' > /var/log/app/app.log"
+sudo su -c "echo 'Info: Started' > /var/log/app/info.log"
+sudo su -c "echo 'Warning: High memory' > /var/log/app/debug.log"
