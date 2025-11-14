ba497cb82e117c436465f79584f22b0e40387404
anil kumar <akthinq@gmail.com>
2025-11-14 18:59:53 +0530

    back-file

--- 01-shell/backup-logs.sh
+++ 01-shell/backup-logs.sh
@@ -29,10 +29,39 @@ RESET="\e[0m"
 
 USER=$(id -u)
 
-if [ $USER -ne 0 ]
+SOURCE_DIR="var/log/app"
+DESTINATION_DIR="opt/backups"
+RETENTION_DAYS=7
+LOG_FILE="var/log/backup_script.log"
+
+# check user is admin or not
+if [ $USER -ne 0 ];
 then
     echo "need to excecute the script with admin right"
     exit 1
 else
     echo "script is stareted excecuting"
 fi
+
+#  check override parameters are passed or not
+if [ ! -z $1 ];
+then 
+    SOURCE_DIR=$1;
+fi
+
+if [ ! -z $2 ];
+then 
+    DESTINATION_DIR=$2;
+fi
+
+if [ ! -z $3 ];
+then 
+    RETENTION_DAYS=$3;
+fi
+
+# Time stamps
+TIME=$(date +"%Y-%m-%d_%H-%M-%S")
+BACKUP_FILE="logs_$TIME.tar.gz"
+
+# confirm backup started
+echo -e "${YELLOW}Starting Log Backup...${RESET}" | tee -a $LOG_FILE
